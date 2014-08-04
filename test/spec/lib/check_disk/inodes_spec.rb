04565137730040ee0576ef4830816159809a3589
Miah Johnson <miah@chia-pet.org>
2014-08-04 16:04:31 -0700

    clean up code a bit.

--- test/spec/lib/check_disk/inodes_spec.rb
+++ test/spec/lib/check_disk/inodes_spec.rb
@@ -12,11 +12,11 @@ describe CheckDisk, '' do
     }
   end
   let(:filesystem) do
-    {
+    OpenStruct.new(
       files: 121_846_308,
       files_available: 42_461_553,
       files_free: 42_461_553
-    }
+    )
   end
 
   before :each do
@@ -32,28 +32,28 @@ describe CheckDisk, '' do
   describe 'Discovers filesystem details.' do
     it 'Can find total number of inodes.' do
       @check.must_respond_to(:total)
-      @check.stub :path_stat, (OpenStruct.new(filesystem)) do
+      @check.stub :path_stat, (filesystem) do
         @check.total.must_equal(121_846_308)
       end
     end
 
     it 'Can find inodes available.' do
       @check.must_respond_to(:available)
-      @check.stub :path_stat, (OpenStruct.new(filesystem)) do
+      @check.stub :path_stat, (filesystem) do
         @check.available.must_equal(42_461_553)
       end
     end
 
     it 'Can find inodes used.' do
       @check.must_respond_to(:used)
-      @check.stub :path_stat, (OpenStruct.new(filesystem)) do
+      @check.stub :path_stat, (filesystem) do
         @check.used.must_equal(79_384_755)
       end
     end
 
     it 'Can find percent of inodes used.' do
       @check.must_respond_to(:percent_used)
-      @check.stub :path_stat, (OpenStruct.new(filesystem)) do
+      @check.stub :path_stat, (filesystem) do
         @check.percent_used.must_equal(55)
       end
     end
