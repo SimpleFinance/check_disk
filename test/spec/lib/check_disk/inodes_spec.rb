ffe783a1928726d031f9e47392bc1ba92f16ba58
Miah Johnson <miah@chia-pet.org>
2014-08-05 11:15:39 -0700

    these dont need to be instance variables.

--- test/spec/lib/check_disk/inodes_spec.rb
+++ test/spec/lib/check_disk/inodes_spec.rb
@@ -24,56 +24,52 @@ describe CheckDisk, '' do
     )
   end
 
-  before :each do
-    @check = check
-  end
-
   describe 'Object Ancestry Checks' do
     it 'Is a Sensu CLI Check Plugin?' do
-      @check.must_be_kind_of(CheckDisk::Inode)
+      check.must_be_kind_of(CheckDisk::Inode)
     end
   end
 
   describe 'Discovers filesystem details.' do
     it 'Can find total number of inodes.' do
-      @check.must_respond_to(:total)
-      @check.stub :path_stat, (filesystem) do
-        @check.total.must_equal(121_846_308)
+      check.must_respond_to(:total)
+      check.stub :path_stat, (filesystem) do
+        check.total.must_equal(121_846_308)
       end
     end
 
     it 'Can find inodes available.' do
-      @check.must_respond_to(:available)
-      @check.stub :path_stat, (filesystem) do
-        @check.available.must_equal(42_461_553)
+      check.must_respond_to(:available)
+      check.stub :path_stat, (filesystem) do
+        check.available.must_equal(42_461_553)
       end
     end
 
     it 'Can find inodes used.' do
-      @check.must_respond_to(:used)
-      @check.stub :path_stat, (filesystem) do
-        @check.used.must_equal(79_384_755)
+      check.must_respond_to(:used)
+      check.stub :path_stat, (filesystem) do
+        check.used.must_equal(79_384_755)
       end
     end
 
     it 'Can find percent of inodes used.' do
-      @check.must_respond_to(:percent_used)
-      @check.stub :path_stat, (filesystem) do
-        @check.percent_used.must_equal(65)
+      check.must_respond_to(:percent_used)
+      check.stub :path_stat, (filesystem) do
+        check.percent_used.must_equal(65)
       end
     end
 
     it 'Can tell us if we are warning?' do
-      @check.must_respond_to(:warning?)
-      @check.stub :path_stat, (filesystem) do
-        @check.warning?.must_equal(true)
+      check.must_respond_to(:warning?)
+      check.stub :path_stat, (filesystem) do
+        check.warning?.must_equal(true)
       end
     end
 
     it 'Can tell us if we are critical?' do
-      @check.must_respond_to(:warning?)
-      @check.stub :path_stat, (filesystem) do
-        @check.critical?.must_equal(false)
+      check.must_respond_to(:warning?)
+      check.stub :path_stat, (filesystem) do
+        check.critical?.must_equal(false)
       end
     end
   end
