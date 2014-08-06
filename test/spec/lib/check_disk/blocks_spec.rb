2a0333f4552401d3d751961bdb9c6ce981501512
Miah Johnson <miah@chia-pet.org>
2014-08-05 19:45:53 -0700

    fix requirements.

--- test/spec/lib/check_disk/blocks_spec.rb
+++ test/spec/lib/check_disk/blocks_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../../spec_helper'
-require_relative '../../../../lib/check_disk/blocks'
+require 'check_disk/blocks'
 require 'ostruct'
 
 describe CheckDisk::Block, '' do
--- test/spec/lib/check_disk/inodes_spec.rb
+++ test/spec/lib/check_disk/inodes_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../../spec_helper'
-require_relative '../../../../lib/check_disk/inodes'
+require 'check_disk/inodes'
 require 'ostruct'
 
 describe CheckDisk::Inode, '' do
